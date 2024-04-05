3fcc675b966a6bd328d2100f30e8f1deff447757
Blue <118444162+KitchenTile@users.noreply.github.com>
2024-04-05 14:53:06 +0100

    Add files via upload

--- Components/Header/Header.js
+++ Components/Header/Header.js
@@ -1,17 +1,42 @@
- import React from "react";
+ import React, { useState, useEffect } from "react";
  import Headercss from "./Header.module.css"
 
  const Header = () => {
-     return (
-        <div className={`${Headercss.headertit}`}>
-            <h1 className={`${Headercss.headertitle}`}>
-                <a href="/" className={`${Headercss.headerlink}`}>
-                    FILMLANCERS.LON.COM
-                </a>
-            </h1>
-        </div>
+    //  return (
+    //     <div className={`${Headercss.headertit}`}>
+    //         <h1 className={`${Headercss.headertitle}`}>
+    //             <a href="/" className={`${Headercss.headerlink}`}>
+    //                 FILMLANCERS.LON.COM
+    //             </a>
+    //         </h1>
+    //     </div>
         
-    );
+    // );
+    
+    const [scrollPosition, setScrollPosition] = useState(0);
+    
+    useEffect(() => {
+        const handleScroll = () => {
+          const position = window.scrollY;
+          setScrollPosition(position);
+        };
+    
+        window.addEventListener('scroll', handleScroll, { passive: true });
+    
+        return () => {
+          window.removeEventListener('scroll', handleScroll);
+        };
+      }, []);
+
+    return(
+        <div className={`${Headercss.headertit}`} style={{ fontSize: scrollPosition > 0 ? "2.5em" : "3.5em"}}>
+             <h1 className={`${Headercss.headertitle}`}>
+                 <a href="/" className={`${Headercss.headerlink}`}>
+                     FILMLANCERS.LON.COM
+                 </a>
+             </h1>
+        </div>
+    )
  };
 
 
--- Components/ImagesOnHover/MainPageImage.js
+++ Components/ImagesOnHover/MainPageImage.js
@@ -1,9 +1,11 @@
 import React, { useState } from "react";
-import home_page_photo from "D:/REACTFILMLANCERS/filmlancers/src/images/home_page_photo.jpg";
-import home_page_photo_bw from "D:/REACTFILMLANCERS/filmlancers/src/images/home_page_photo_bw.jpg";
+import home_page_photo from "../../images/RENTALIMAGES/home_page_photo.jpg";
+import home_page_photo_bw from "..//../images/RENTALIMAGES/home_page_photo_bw.jpg";
+
 import './MainPageImage.css';
 
 const ImageHoverComponent = () => {
+
     const [isHovered, setIsHovered] = useState(false);
 
     const handleHoverYes = () => {
@@ -15,22 +17,24 @@ const ImageHoverComponent = () => {
 
     return(
         <div>
-            <h1 className="h1">↓ RENTAL HOUSES ↓</h1>
+            {/* <h1 className="h1">↓ RENTAL HOUSES ↓</h1> */}
             <a
-                href="#"
-                className="image-link"
+                href="/rentalpage"
+                className="Homeimage-link"
                 onMouseEnter={handleHoverYes}
                 onMouseLeave={handleHoverNo}
             >
                 <img
                     src= {isHovered ? home_page_photo_bw : home_page_photo}                
                     // src= {isHovered ? "D:/REACTFILMLANCERS/filmlancers/src/images/home_page_photo_bw.jpg" : "D:/REACTFILMLANCERS/filmlancers/src/images/home_page_photo.jpg"}
-                    
-                    className="image"
+                    alt="img"
+                    className="Homeimage"
                 />
+                
             </a>
         </div>
     );
 };
 
 export default ImageHoverComponent;
+
