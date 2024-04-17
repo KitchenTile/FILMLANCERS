128bed1d613d19cacad69fcd1fe54b4ac9e57d99
Blue <118444162+KitchenTile@users.noreply.github.com>
2024-04-17 12:08:06 +0100

    Add files via upload

---
+++ ReusableComps/MainPageComps.js
@@ -0,0 +1,32 @@
+import React from "react";
+import MainCompsData from "../../data/MainPageData";
+import Compcss from "./Components.module.css";
+import ImageHoverComponent from "../ImagesOnHover/MainPageImage";
+
+const ReusComp = ({ title, text }) => {
+    return (
+        <div className={`${Compcss.components}`}>
+            <h2 className={`${Compcss.title}`}>{title}</h2>
+            <p>{text}</p>
+        </div>
+    )
+};
+
+const MainComponent = () => {
+    return (
+        <div >
+            {MainCompsData.map((component, index) => (
+                <div key={index}>
+                    <ReusComp
+                        title={component.title}
+                        text={component.text}
+                    />
+                {index === MainCompsData.length - 4 && <ImageHoverComponent />}
+                </div>
+            ))}
+        </div>
+    );
+};
+
+
+export default MainComponent;
---
+++ ReusableComps/RentalSearchComp.js
@@ -0,0 +1,83 @@
+import React, { useState } from "react";
+import "./Rentalcomp.css";
+
+const RentalSearchComp = ({ data }) => {
+    const [query, setQuery] = useState("");
+    // const [isHovered, setIsHovered] = useState(false);
+    
+    const filteredData = data.filter(item =>
+        item.name.toLowerCase().includes(query.toLowerCase())
+    );
+
+    // const handleHoverYes = () => {
+    //     setIsHovered(true);
+    // };
+    // const handleHoverNo = () => {
+    //     setIsHovered(false);
+    // };
+
+    
+    return (
+        <div>
+            <div className="box2">
+                <form name="search">
+                    <input
+                        type="text"
+                        placeholder="🔎︎"
+                        className="input"
+                        value={query}
+                        onChange={(e) => setQuery(e.target.value)}
+                    />
+                </form>
+            </div>
+            {query !== "" ? (<h1 className="titles">SEARCH RESULTS</h1>): null}
+            {query !== "" && filteredData.length > 0 ? (
+                filteredData.map((item, index) => (
+                    <div 
+                    key={index} 
+                    className="box"
+                    // onMouseEnter={handleHoverYes}
+                    // onMouseLeave={handleHoverNo}
+                    >
+                        <a 
+                        href={item.link}
+                        className="image-link"
+                        > 
+                            <img 
+                            src={item.img}
+                            className="image-link"
+                            />
+                        
+                        </a>
+                            {/* {isHovered && ( */}
+                                <div className="content">
+                                    <div className="info">
+                                        <p>{item.info}</p>
+                                    </div>
+                                    <div className="reviews">
+                                        <h1>Reviews</h1>
+                                    </div>
+                                    <div className="map">
+                                        <iframe 
+                                            src={item.map}
+                                            width="80%"
+                                            height="70%"
+                                            style={{border:"0"}}
+                                            allowfullscreen=""
+                                            loading="lazy"
+                                            referrerpolicy="no-referrer-when-downgrade"
+                                        /> 
+                                    </div>
+                                </div>
+                            {/* )}  */}
+                    </div>
+                ))
+            ) : (
+                query === "" ? null : <div className="nrf">No results found</div>
+            )}
+        </div>
+    );
+
+};
+
+export default RentalSearchComp;
